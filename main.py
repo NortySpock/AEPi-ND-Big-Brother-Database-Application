9f8b765f494513376572fc934becc809ef7a0d2b
David Norton <david.k.norton@gmail.com>
2011-02-26 14:55:56 -0600

    Changed window initial geometry.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PyQt4 import QtGui
 app = QtGui.QApplication(sys.argv)
 
 widget = QtGui.QWidget()
-widget.resize(250, 150)
+widget.setGeometry(100, 100, 600, 400) #x offset, y offset, x length, y length
 widget.setWindowTitle('AEPi ND Big Brother Database')
 widget.show()
 
