dd467c2b1c14975d4bdee467689c98417bdbca49
David Norton <david.k.norton@gmail.com>
2011-03-08 03:32:23 -0600

    Fleshed out what the frontend code would look like.

--- AEPi-Big-Brother-Database-Application/bb_db_frontend.cpp
+++ AEPi-Big-Brother-Database-Application/bb_db_frontend.cpp
@@ -25,8 +25,66 @@ void bb_db_frontend::changeEvent(QEvent *e)
     }
 }
 
+//Make me a backend!
+backend be;
 
+//User picked database settings
+void bb_db_frontend::on_confirmDBButton_clicked()
+{
+    // Wants a fresh DB
+    if(emptyDBRadioButton.checked() == TRUE)
+    {
+        be.initDB();
+        be.resetDB();
+        be.createTables();
+    }
+    else if(mockDBRadioButton.checked() == TRUE)
+    {
+        be.initDB();
+        be.resetDB();
+        be.createTables();
+        for(Iterator iter = hypotheticalMockLinkedListOfMemberObjects.begin(); iter != hypotheticalMockList.end(); iter++)
+        {
+            be.add(*iter);
+        }
+    }
+    else if(csvDBRadioButton.checked() == TRUE)
+    {
+        be.initDB();
+        be.resetDB();
+        be.createTables();
+        hypotheticalLinkedListOfMemberObjects = //python command to parse a particular layout of CSV stuff and give back a list.
+        for(Iterator iter = hypotheticalListOfMemberObjects.begin(); iter != hypotheticalMockList.end(); iter++)
+        {
+            be.add(*iter);
+        }
+    }
+    else //Use existing DB. The horror!
+    {
+        be.initDB();
+    }
+}
+
+//User wants to find a member
 void bb_db_frontend::on_findMemberMemberButton_clicked()
+{
+    if()
+}
+
+//User wants to update a member
+void bb_db_frontend::on_submitMemberMemberButton_clicked()
+{
+
+}
+
+//User wants to find a big brother
+void bb_db_frontend::on_findMemberBigBrotherButton_clicked()
+{
+
+}
+
+//User wants to update a big brother
+void bb_db_frontend::on_submitMemberBigBrotherButton_clicked()
 {
 
 }
